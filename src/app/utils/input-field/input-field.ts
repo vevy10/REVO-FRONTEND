32d1c6788c92a6ffedd1e6308e2121db6bde6c4d
vevy10 <gervais.vahanjakoto@gmail.com>
2025-10-17 12:29:10 +0300

    Creation du composant reutilisable input-field

---
+++ src/app/utils/input-field/input-field.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-field',
+  imports: [],
+  template: `
+    <p>
+      input-field works!
+    </p>
+  `,
+  styleUrl: './input-field.css'
+})
+export class InputField {
+
+}
