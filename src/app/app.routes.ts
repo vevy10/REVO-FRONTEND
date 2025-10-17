89f395ae64ccff45ac666c9c574641cf62685f19
vevy10 <gervais.vahanjakoto@gmail.com>
2025-10-17 14:55:31 +0300

    Ajout de sign up dans le route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { SignIn } from './pages/sign-in/sign-in';
+import { SignUp } from './pages/sign-up/sign-up';
 
 export const routes: Routes = [
     {
         path: 'sign-in',
         component: SignIn,
         title: 'REVO | se connecter'
+    },
+    {
+        path: 'sign-up',
+        component: SignUp,
+        title: "REVO | s'inscrire"
     }
 ];
