d28d0fd7d19e5a6adf042ce4768fd0e821fc1f52
vevy10 <gervais.vahanjakoto@gmail.com>
2025-10-17 12:58:54 +0300

    Ajout des routes pour sign-in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignIn } from './pages/sign-in/sign-in';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'sign-in',
+        component: SignIn,
+        title: 'REVO | se connecter'
+    }
+];
